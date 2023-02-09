1f443cc9aca81a25ce16242c248c6873b1aad203
NicDUBS <nicolas.dubois@iddi.com>
2023-02-09 13:11:50 +0000

    Test je dis bonjour à Soph

--- programs/adsl.R
+++ programs/adsl.R
@@ -2,6 +2,7 @@
 #
 # Label: Subject Level Analysis Dataset
 #
+# Bonjour Sophie
 # Input: dm, ex, ds
 library(admiral)
 library(admiral.test) # Contains example datasets from the CDISC pilot project
